89fc865ef6df65341c4a9f6c6ae325d9e9002ae7
petros <pit.trak@gmail.com>
2020-07-21 15:20:07 +0300

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import Projects from './components/Projects';
 import About from './components/About';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -15,24 +16,7 @@ function App() {
       <Services />
       <Projects />
       <About />
-      <section id="footer">
-        <div className="footer container">
-          <div className="brand"><h1><span>P</span>etros <span>T</span>rakadas</h1></div>
-          <h2>You can reach me at..</h2>
-          <div className="social-icon">
-            <div className="social-item">
-              <a href="https://www.linkedin.com/in/petrostrak/" target="_blank"><img src="https://img.icons8.com/bubbles/100/000000/linkedin.png" alt="" /></a>
-            </div>
-            <div className="social-item">
-              <a href="https://github.com/petrostrak" target="_blank"><img src="https://img.icons8.com/bubbles/100/000000/github.png" alt="" /></a>
-            </div>
-            <div className="social-item">
-              <a href="mailto:pit.trak@gmail.com"><img src="https://img.icons8.com/bubbles/100/000000/email.png" alt="" /></a>
-            </div>
-          </div>
-          <p>Copyright © 2020 Petros. All rights reserved</p>
-        </div>
-      </section>
+      <Footer />
       <script src="./app.js"></script>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => (
+    <section id="footer">
+        <div className="footer container">
+            <div className="brand"><h1><span>P</span>etros <span>T</span>rakadas</h1></div>
+            <h2>You can reach me at..</h2>
+            <div className="social-icon">
+                <div className="social-item">
+                    <a href="https://www.linkedin.com/in/petrostrak/" target="_blank"><img src="https://img.icons8.com/bubbles/100/000000/linkedin.png" alt="" /></a>
+                </div>
+                <div className="social-item">
+                    <a href="https://github.com/petrostrak" target="_blank"><img src="https://img.icons8.com/bubbles/100/000000/github.png" alt="" /></a>
+                </div>
+                <div className="social-item">
+                    <a href="mailto:pit.trak@gmail.com"><img src="https://img.icons8.com/bubbles/100/000000/email.png" alt="" /></a>
+                </div>
+            </div>
+            <p>Copyright © 2020 Petros. All rights reserved</p>
+        </div>
+    </section>
+);
+
+export default Footer;
